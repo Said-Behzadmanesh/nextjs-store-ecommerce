66a39a46302d39eb812508e0c8922c73eb37520d
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-10-07 02:20:38 +0200

    basic header and footer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,22 @@
+import Footer from "@/components/footer";
+import Header from "@/components/shared/header";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Prostore",
+  description: "Ecommerce with Nextjs",
+};
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <Header />
+      <main className="flex-1 wrapper">{children}</main>
+      <Footer />
+    </div>
+  );
+}
