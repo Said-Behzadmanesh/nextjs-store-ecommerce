e134ef1618db3e5101ec90eec95929d54bb00f73
Said Behzadmanesh <behmanesh_s@yahoo.com>
2025-10-14 10:46:23 +0200

    JWT

--- auth.ts
+++ auth.ts
@@ -53,6 +53,9 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // set the user id from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+
 
       // if there is an update set the user name
       if (trigger === "update") {
@@ -60,6 +63,28 @@ export const config = {
       }
 
       return session;
+    },
+
+    async jwt({ token, user, trigger, session }: any) {
+      if (user) {
+        token.role = user.role;
+        // if user has no name the use the email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+
+          // update database to reflect the token name
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              name: token.name,
+            },
+          });
+        }
+
+      }
+      return token;
     },
   },
 } satisfies NextAuthConfig;
